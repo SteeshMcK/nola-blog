ce885a3e3a97f3dc328c297715a8164a690e0d01
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-10 21:23:01 -0800

    create Home.css create hardcoded state and dynamic html template

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,35 @@
-const Home = () => {
+import { useState } from 'react'
+import './Home.css'
 
-    const handleClick = (e) => {
-        console.log("Where y'at?", e.target)
+const hardCodeBlogs = [
+    {title: 'I wish I was in New Orleans',
+     body: 'Well, I wish I was in New Orleans, I can see it in my dreams, Arm-in-arm down Burgundy, a bottle and my friends and me',
+     author: 'Tom',
+     id: 1
+    },
+    {title: 'Sweet Home New Orleans',
+     body: 'Sweet Home New Orleans. I can sure \'nuff hear you calling. Feel that gentle rain be falling. Say \'Where y\'at my darling.',
+     author: 'Dr. John',
+     id: 2
+    },
+    {title: 'Zydeco sont pas sale',
+     body: 'T\'as volé mon gilet, T\'as volé mon chapeau, Garde hip et taïaut, Les haricots sont pas salés.',
+     author: 'Monsieur Chenier',
+     id: 3
     }
+]
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(hardCodeBlogs)
+    
     return ( 
         <div className='home'>
-            <h2>Home</h2>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className='blog-preview'>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
