d3c0132db68f12786a58f3db7aecf8e154d4ee53
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-11 21:59:31 -0800

    create BlogList component and nest inside Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import BlogList from '../BlogList/BlogList'
 import './Home.css'
 
 const hardCodeBlogs = [
@@ -24,14 +25,9 @@ const Home = () => {
     
     return ( 
         <div className='home'>
-            {blogs.map((blog) => (
-                <div className='blog-preview'>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={ blogs } title="All Blogs!" />
         </div>
      );
 }
  
-export default Home;
+export default Home
