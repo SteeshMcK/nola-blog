b977a3ddbe3cdcc989ee58f76d403ae7ce1813de
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-12 21:41:07 -0800

    add a delete button with functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,31 +2,34 @@ import { useState } from 'react'
 import BlogList from '../BlogList/BlogList'
 import './Home.css'
 
-const hardCodeBlogs = [
-    {title: 'I wish I was in New Orleans',
-     body: 'Well, I wish I was in New Orleans, I can see it in my dreams, Arm-in-arm down Burgundy, a bottle and my friends and me',
-     author: 'Tom',
-     id: 1
-    },
-    {title: 'Sweet Home New Orleans',
-     body: 'Sweet Home New Orleans. I can sure \'nuff hear you calling. Feel that gentle rain be falling. Say \'Where y\'at my darling.',
-     author: 'Dr. John',
-     id: 2
-    },
-    {title: 'Zydeco sont pas sale',
-     body: 'T\'as volé mon gilet, T\'as volé mon chapeau, Garde hip et taïaut, Les haricots sont pas salés.',
-     author: 'Monsieur Chenier',
-     id: 3
-    }
-]
-
 const Home = () => {
+    const hardCodeBlogs = [
+        {title: 'I wish I was in New Orleans',
+         body: 'Well, I wish I was in New Orleans, I can see it in my dreams, Arm-in-arm down Burgundy, a bottle and my friends and me',
+         author: 'Tom',
+         id: 1
+        },
+        {title: 'Sweet Home New Orleans',
+         body: 'Sweet Home New Orleans. I can sure \'nuff hear you calling. Feel that gentle rain be falling. Say \'Where y\'at my darling.',
+         author: 'Dr. John',
+         id: 2
+        },
+        {title: 'Zydeco sont pas sale',
+         body: 'T\'as volé mon gilet, T\'as volé mon chapeau, Garde hip et taïaut, Les haricots sont pas salés.',
+         author: 'Monsieur Chenier',
+         id: 3
+        }
+    ]
     const [blogs, setBlogs] = useState(hardCodeBlogs)
-    
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className='home'>
-            <BlogList blogs={ blogs } title="All Blogs!" />
-            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Tom') } title="Tom's Blogs!" />
+            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete } />
         </div>
      );
 }
