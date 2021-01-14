c5d84d9568e88fc92b98770070808b4bd6ab5c13
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-13 21:16:11 -0800

    add fetch method to call data from json file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,41 +3,23 @@ import BlogList from '../BlogList/BlogList'
 import './Home.css'
 
 const Home = () => {
-    const hardCodeBlogs = [
-        {title: 'I wish I was in New Orleans',
-         body: 'Well, I wish I was in New Orleans, I can see it in my dreams, Arm-in-arm down Burgundy, a bottle and my friends and me',
-         author: 'Tom',
-         id: 1
-        },
-        {title: 'Sweet Home New Orleans',
-         body: 'Sweet Home New Orleans. I can sure \'nuff hear you calling. Feel that gentle rain be falling. Say \'Where y\'at my darling.',
-         author: 'Dr. John',
-         id: 2
-        },
-        {title: 'Zydeco sont pas sale',
-         body: 'T\'as volé mon gilet, T\'as volé mon chapeau, Garde hip et taïaut, Les haricots sont pas salés.',
-         author: 'Monsieur Chenier',
-         id: 3
-        }
-    ]
-    const [blogs, setBlogs] = useState(hardCodeBlogs)
-    const [name, setName] = useState('Stesha')
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('useEffect ran')
-        console.log(name)
-    }, [name])
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setBlogs(data)
+            })
+    }, [])
 
     return ( 
         <div className='home'>
-            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete } />
-            <button onClick={() => setName('LaStesha, the Carnival Queen')}>Surprise!</button>
-            <p>{ name }</p>
+            {/*Below is 'conditional templating" in React*/}
+            {blogs && <BlogList blogs={ blogs } title="All Blogs!" />}
         </div>
      );
 }
