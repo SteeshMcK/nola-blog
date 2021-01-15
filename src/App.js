f42f4694c8aee0d3826156b70f97122d0dabddb3
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-14 21:21:55 -0800

    install react-router-dom wrap home component with a route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   
   return (
-    <div className="App">
-    <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
