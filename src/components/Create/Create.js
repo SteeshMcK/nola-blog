f2275eecb86448740fa566922cdb971618547c7f
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-19 20:45:37 -0800

    add form to create.js create.css

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,6 +2,24 @@ const Create = () => {
     return ( 
         <div className='create'>
             <h2>Tell Me Somethin', Mistah!</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type='text'
+                    required
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value='Tom'>Tom</option>
+                    <option value='Dr. John'>Dr. John</option>
+                    <option value='Queen LaStesha'>Queen LaStesha</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
