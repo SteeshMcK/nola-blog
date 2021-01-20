30b7db04ef0751988968f36eb880e23cd6301e9f
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-19 21:09:31 -0800

    add state and onChange events to form fields

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,4 +1,11 @@
+import './Create.css'
+import { useState } from 'react'
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('Tom')
+
     return ( 
         <div className='create'>
             <h2>Tell Me Somethin', Mistah!</h2>
@@ -7,13 +14,20 @@ const Create = () => {
                 <input
                     type='text'
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value='Tom'>Tom</option>
                     <option value='Dr. John'>Dr. John</option>
                     <option value='Queen LaStesha'>Queen LaStesha</option>
