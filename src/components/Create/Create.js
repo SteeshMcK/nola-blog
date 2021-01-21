e7ec27c842b572fa24f8bd2b4c63fc4e974c84f4
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-21 13:36:22 -0800

    install useHistory hook to redirect from /create to home page

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,11 +1,13 @@
 import './Create.css'
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('Tom')
     const [isLoading, setIsLoading] = useState(false)
+    const history = useHistory()
 
     const handleSubmit =(e) => {
         e.preventDefault()
@@ -20,6 +22,7 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added')
             setIsLoading(false)
+            history.push('/')
         })  
         console.log(blog)
     }
